13c88287329d8e29672b610e02018125762a1513
Liam MacNeil <32491405+DakKhuza@users.noreply.github.com>
2018-08-04 00:59:32 -0700

    Dependency Updates
    Added dependencies for sqlite and entity framework in prep for adding the database.

---
+++ FortunaUnofficalAPI/FortunaUnofficalAPI/Data/FortunaDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.EntityFrameworkCore;
+using FortunaUnofficalAPI.Models;
+
+namespace FortunaUnofficalAPI.Data
+{
+    public class FortunaDbContext : DbContext
+    {
+        public DbSet<String> SpeciesUrls {get; set;}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=fortuna.db");
+        }
+    }
+}

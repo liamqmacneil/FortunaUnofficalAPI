cfd10a66e12eebfe74ca538fd991b85ca522bc99
Liam MacNeil <32491405+DakKhuza@users.noreply.github.com>
2018-09-21 00:56:39 -0700

    Removed database update api route and added DatabaseBuilder class to replace its functionality.

--- FortunaUnofficalAPI/FortunaUnofficalAPI/Controllers/UpdateController.cs
+++ FortunaUnofficalAPI/FortunaUnofficalAPI/Controllers/UpdateController.cs
@@ -5,29 +5,45 @@ using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace FortunaUnofficialAPI.Controllers
 {
     public class UpdateController : ApiController
     {
-        public SpeciesContainer Test()
+         /*public SpeciesContainer Test()
+         {
+             AttributesGeneric baseObject = new AttributesGeneric();
+             WebClient client = new WebClient();
+             var testList = new List<string>();
+             string pattern = "(\\<a href=\"([a-zA-Z0-9\\-]*)\"\\>)";
+             string downloadedString = client.DownloadString("https://cosmosdex.com/cosmosdex/fauna/");
+             foreach (Match m in Regex.Matches(downloadedString,pattern))
+             {
+                 string speciesLink = "https://cosmosdex.com/cosmosdex/fauna/" + Regex.Match(m.Value, "(\"([a-zA-Z0-9-]*))").Value.Remove(0, 1);
+             }
+             using (var _db = new FortunaDbContext())
+             {
+                 var ResponseSpeciesContainer = _db.SpeciesContainers.Where(a => a.Url == "MainUrl").Include(b => b.SpeciesAttributes).Include(c => c.GenericAttributes).Include(d => d.GenericAttributes.ArticleCreator).Include(e => e.GenericAttributes.ArticleRewriter).FirstOrDefault();
+                 return ResponseSpeciesContainer;
+             }
+         }*/
+        
+        
+        /*public async Task<IHttpActionResult> UpdateSpecies()
         {
-            /*AttributesGeneric baseObject = new AttributesGeneric();
             WebClient client = new WebClient();
-            var testList = new List<string>();
+            var SpeciesUrls = new List<string>();
             string pattern = "(\\<a href=\"([a-zA-Z0-9\\-]*)\"\\>)";
             string downloadedString = client.DownloadString("https://cosmosdex.com/cosmosdex/fauna/");
-            foreach (Match m in Regex.Matches(downloadedString,pattern))
+            foreach (Match m in Regex.Matches(downloadedString, pattern))
             {
-                string speciesLink = "https://cosmosdex.com/cosmosdex/species/" + Regex.Match(m.Value, "(\"([a-zA-Z0-9-]*))").Value.Remove(0, 1);
-                
-            }*/
-            using (var _db = new FortunaDbContext())
-            {
-                var ResponseSpeciesContainer = _db.SpeciesContainers.Where(a => a.Url == "MainUrl").Include(b => b.SpeciesAttributes).Include(c => c.GenericAttributes).Include(d => d.GenericAttributes.ArticleCreator).Include(e => e.GenericAttributes.ArticleRewriter).FirstOrDefault();
-                return ResponseSpeciesContainer;
+                string ToProcess = await client.DownloadStringTaskAsync("https://cosmosdex.com/cosmosdex/fauna/" + Regex.Match(m.Value, "(\"([a-zA-Z0-9-]*))").Value.Remove(0, 1));
             }
-        }
+            return Ok();
+        }*/
     }
 }
---
+++ FortunaUnofficalAPI/FortunaUnofficalAPI/Data/DatabaseBuilder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace FortunaUnofficalAPI.Data
+{
+    public class DatabaseBuilder
+    {
+        public static bool BuildSpeciesDatabase()
+        {
+            WebClient client = new WebClient();
+            var SpeciesUrls = new List<string>();
+            string pattern = "(\\<a href=\"([a-zA-Z0-9\\-]*)\"\\>)";
+            string downloadedString = client.DownloadString("https://cosmosdex.com/cosmosdex/species/");
+            foreach (Match m in Regex.Matches(downloadedString, pattern))
+            {
+                //string ToProcess = client.DownloadString("https://cosmosdex.com/cosmosdex/fauna/" + Regex.Match(m.Value, "(\"([a-zA-Z0-9-]*))").Value.Remove(0, 1));
+                System.Diagnostics.Debug.WriteLine(Regex.Match(m.Value, "(\"([a-zA-Z0-9-]*))").Value.Remove(0, 1));
+            }
+            return true;
+        }
+    }
+}

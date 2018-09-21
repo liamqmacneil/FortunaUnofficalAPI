c65f23bd6466288b51913ca36210abe055888bbe
Liam MacNeil <32491405+DakKhuza@users.noreply.github.com>
2018-09-21 00:56:58 -0700

    Removed test data and added DatabaseBuilder call

--- FortunaUnofficalAPI/FortunaUnofficalAPI/App_Start/WebApiConfig.cs
+++ FortunaUnofficalAPI/FortunaUnofficalAPI/App_Start/WebApiConfig.cs
@@ -1,4 +1,5 @@
-﻿using FortunaUnofficalAPI.Models.General;
+﻿using FortunaUnofficalAPI.Data;
+using FortunaUnofficalAPI.Models.General;
 using FortunaUnofficialAPI.Models.Containers;
 using FortunaUnofficialAPI.Models.General;
 using FortunaUnofficialAPI.Models.SAF;
@@ -17,60 +18,10 @@ namespace FortunaUnofficialAPI
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            DatabaseBuilder.BuildSpeciesDatabaseAsync();
 
             // Web API routes
             config.MapHttpAttributeRoutes();
-
-            using (var dataContext = new FortunaDbContext())
-            {
-                dataContext.SpeciesContainers.Add(
-                    new SpeciesContainer()
-                    {
-                        SpeciesAttributes = new AttributesSpecies()
-                        {
-                            Diet = "TestDiet",
-                            Jobs = "JEBS",
-                            LifeSpan = "none",
-                            Rarity = "everywhere",
-                            SocialClass = "none exsistant"
-                        },
-                        GenericAttributes = new AttributesGeneric()
-                        {
-                            Alias = "Alias / Alias",
-                            Blurb = "blurb",
-                            ArticleCreator = new AttributeCreator()
-                            {
-                                CreatorName = "name",
-                                CreatorUrl = "url"
-                            },
-                            ArticleRewriter = new AttributeRewriter()
-                            {
-                                Rewriter = "Rewriter Name",
-                                RewriterUrl = "Url"
-                            },
-                            Name = "TestNme",
-                            MainArt = "mainarturl",
-                            Quote = "Quote",
-                            AltArt = new List<AttributeAltArt> {
-                                new AttributeAltArt() {
-                                    Description = "desc",
-                                    Url = "AltArtURL",
-                                    AltArtCreator = new AttributeCreator()
-                                    {
-                                        Id = Guid.NewGuid(),
-                                        CreatorName = "AltArtCreatorName",
-                                        CreatorUrl = "AltArtCreatorUrl"
-                                    }
-                                }
-                            }
-                        },
-                        Url = "MainUrl"
-                    }
-                );
-                dataContext.SaveChanges();
-            }
-
-
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
